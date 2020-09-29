6a682c96ef72bfd32b5694233d13f1d2b93b3479
Max Block <maxblock@pm.me>
2020-09-29 21:22:28 +0300

    update

--- README.txt
+++ README.txt
@@ -1,4 +1,4 @@
 https://doc.rust-lang.org/book/
 
 
-https://doc.rust-lang.org/book/ch10-00-generics.html
+https://doc.rust-lang.org/book/ch10-03-lifetime-syntax.html
--- tmp/src/main.rs
+++ tmp/src/main.rs
@@ -1,13 +1,23 @@
-use std::collections::HashMap;
-fn main() {
-    let text = "hello world wonderful world";
+fn largest<T: PartialOrd>(list: &[T]) -> &T {
+    let mut largest = &list[0];
 
-    let mut map = HashMap::new();
-
-    for word in text.split_whitespace() {
-        let count = map.entry(word).or_insert(0);
-        *count += 1;
+    for item in list {
+        if item > largest {
+            largest = item;
+        }
     }
 
-    println!("{:?}", map);
+    largest
+}
+
+fn main() {
+    let number_list = vec![34, 50, 25, 100, 65];
+
+    let result = largest(&number_list);
+    println!("The largest number is {}", result);
+
+    let char_list = vec!['y', 'm', 'a', 'q'];
+
+    let result = largest(&char_list);
+    println!("The largest char is {}", result);
 }
